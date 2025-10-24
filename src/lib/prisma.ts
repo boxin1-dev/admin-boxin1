9cadde0c8b38c1f3accbfdca90a1a0c327ae1b68
NiainaTR <tsantaniainaraherison@gmail.com>
2025-10-24 10:52:07 +0300

    fix import prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../generated/client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
