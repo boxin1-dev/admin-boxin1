6ccc9d740aa30c0637fa9f2a8f9fdd9237827fb6
NiainaTR <tsantaniainaraherison@gmail.com>
2025-10-27 09:33:22 +0300

    setup dashboard

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -18,11 +18,5 @@ export default function layout({
 
   //const userRole = session?.user.role as Role;
 
-  return (
-    <div className="flex h-screen bg-background">
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <main className="flex-1 overflow-auto p-6">{children}</main>
-      </div>
-    </div>
-  );
+  return <>{children}</>;
 }
