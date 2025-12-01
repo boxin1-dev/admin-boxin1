bbef9f91286e107c99686d95d1f2fa9d5ee1775b
NiainaTR <tsantaniainaraherison@gmail.com>
2025-12-01 18:09:55 +0300

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
-import * as path from "path";
 import { defineConfig, env } from "prisma/config";
 
 type Env = {
   DATABASE_URL: string;
 };
 export default defineConfig({
-  engine: "classic",
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
     url: env<Env>("DATABASE_URL"),
   },
-  schema: path.join("prisma", "schema.prisma"),
 });
